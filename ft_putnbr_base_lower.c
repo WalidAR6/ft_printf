eb55553612836f2e80169eb98183ef5aad33c794
Walid Araissi <waraissi@e1r4p2.1337.ma>
2022-11-09 19:12:34 +0100

    write numbers in hexadecimal lower %x (base 16)

---
+++ ft_putnbr_base_lower.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base_lower.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: waraissi <waraissi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 18:42:57 by waraissi          #+#    #+#             */
+/*   Updated: 2022/11/09 18:48:44 by waraissi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "printf.h"
+
+void ft_putnbr_base_lower(unsigned int nbr, int *res)
+{
+	char base[] = "0123456789abcdef";
+	if (nbr < 16)
+			ft_putchar(base[nbr], res);
+	else
+	{
+		ft_putnbr_base_lower(nbr / 16, res);
+		ft_putnbr_base_lower(nbr % 16, res);
+	}
+}
