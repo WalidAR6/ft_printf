4ad8c9671c2abe20ce699cc5775e5e47da86d446
Walid Araissi <waraissi@e1r4p2.1337.ma>
2022-11-09 20:18:04 +0100

    write numbers in hexadecimal

--- ft_putnbr_base.c
+++ ft_putnbr_base.c
@@ -6,11 +6,11 @@
 /*   By: waraissi <waraissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 18:42:39 by waraissi          #+#    #+#             */
-/*   Updated: 2022/11/09 18:48:54 by waraissi         ###   ########.fr       */
+/*   Updated: 2022/11/09 20:17:11 by waraissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
+#include "ft_printf.h"
 
 void ft_putnbr_base(unsigned long long nbr, int *res)
 {
