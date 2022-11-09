2265b714e3757b4b1975e661f9b08ebe37c34466
Walid Araissi <waraissi@e1r4p2.1337.ma>
2022-11-09 19:00:04 +0100

    header file

---
+++ ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: waraissi <waraissi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 18:43:39 by waraissi          #+#    #+#             */
+/*   Updated: 2022/11/09 18:54:21 by waraissi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF
+# define FT_PRINTF
+
+# include<unistd.h>
+# include<stdarg.h>
+
+void	ft_putchar(char c , int *res);
+void	ft_putstr(char *s, int *res);
+void	ft_putnbr(int n, int *res);
+void	ft_putnbr_unsigned(unsigned int nbr, int *res);
+void	ft_putnbr_base(unsigned long long nbr, int *res);
+void	ft_putnbr_base_lower(unsigned int nbr, int *res);
+void	ft_putnbr_base_upper(unsigned int nbr, int *res);
+int 	ft_printf(const char *str, ...);
+
+#endif
