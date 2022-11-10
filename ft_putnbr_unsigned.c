a5f3712587fdec9cadcfeda4782648f794c9a251
Walid Araissi <waraissi@e1r5p4.1337.ma>
2022-11-10 18:15:12 +0100

    write unsigned numbers

--- ft_putnbr_unsigned.c
+++ ft_putnbr_unsigned.c
@@ -6,13 +6,13 @@
 /*   By: waraissi <waraissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 18:42:25 by waraissi          #+#    #+#             */
-/*   Updated: 2022/11/09 21:33:00 by waraissi         ###   ########.fr       */
+/*   Updated: 2022/11/10 18:14:55 by waraissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void ft_putnbr_unsigned(unsigned int nbr, int *res)
+void	ft_putnbr_unsigned(unsigned int nbr, int *res)
 {
 	if (nbr < 10)
 		ft_putchar(nbr + 48, res);
