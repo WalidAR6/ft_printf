100aa98706ac1761573f892df0095b3e690d99ca
Walid Araissi <waraissi@e1r5p4.1337.ma>
2022-11-10 18:14:24 +0100

    ft_putnbr base

--- ft_putnbr_base.c
+++ ft_putnbr_base.c
@@ -6,18 +6,19 @@
 /*   By: waraissi <waraissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 18:42:39 by waraissi          #+#    #+#             */
-/*   Updated: 2022/11/10 17:29:22 by waraissi         ###   ########.fr       */
+/*   Updated: 2022/11/10 18:12:12 by waraissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void ft_putnbr_base(unsigned long long nbr, int *res)
+void	ft_putnbr_base(unsigned long long nbr, int *res)
 {
-	char base[] = "0123456789abcdef";
-	
+	char	*base;
+
+	base = "0123456789abcdef";
 	if (nbr < 16)
-			ft_putchar(base[nbr], res);
+		ft_putchar(base[nbr], res);
 	else
 	{
 		ft_putnbr_base(nbr / 16, res);
