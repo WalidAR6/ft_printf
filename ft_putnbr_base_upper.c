3d454a7cecf1a05429749fa0a48b2349af627900
Walid Araissi <waraissi@e1r5p4.1337.ma>
2022-11-10 18:10:44 +0100

    write numbers in hexadecimal upper %X (base 16)

--- ft_putnbr_base_upper.c
+++ ft_putnbr_base_upper.c
@@ -6,20 +6,22 @@
 /*   By: waraissi <waraissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 18:43:07 by waraissi          #+#    #+#             */
-/*   Updated: 2022/11/09 20:16:28 by waraissi         ###   ########.fr       */
+/*   Updated: 2022/11/10 18:10:06 by waraissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void ft_putnbr_base_upper(unsigned int nbr, int *res)
+void	ft_putnbr_base_upper(unsigned int nbr, int *res)
 {
-	char base[] = "0123456789ABCDEF";
+	char	*base;
+
+	base = "0123456789ABCDEF";
 	if (nbr < 16)
-			ft_putchar(base[nbr], res);
+		ft_putchar(base[nbr], res);
 	else
 	{
-		ft_putnbr_base_upper(nbr / 16, res );
+		ft_putnbr_base_upper(nbr / 16, res);
 		ft_putnbr_base_upper(nbr % 16, res);
 	}
 }
